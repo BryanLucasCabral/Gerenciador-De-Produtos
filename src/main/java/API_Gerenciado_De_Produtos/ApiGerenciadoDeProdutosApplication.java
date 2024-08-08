953874c7cd730ef54ddb3fc9f17523411c1b6f96
BryanLucasCabral <bryan.brasil.bl@gmail.com>
2024-08-08 20:56:49 -0300

    Projeto criado

---
+++ src/main/java/API_Gerenciado_De_Produtos/ApiGerenciadoDeProdutosApplication.java
@@ -0,0 +1,13 @@
+package API_Gerenciado_De_Produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerenciadoDeProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGerenciadoDeProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/API_Gerenciado_De_Produtos/ApiGerenciadoDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package API_Gerenciado_De_Produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGerenciadoDeProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
