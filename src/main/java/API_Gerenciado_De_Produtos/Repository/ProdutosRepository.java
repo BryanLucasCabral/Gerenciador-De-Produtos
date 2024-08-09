2baf5397f4d26db0d23137b09d6a3462c840d9c3
BryanLucasCabral <bryan.brasil.bl@gmail.com>
2024-08-08 21:25:56 -0300

    Adicionado a notacao @Repository

--- src/main/java/API_Gerenciado_De_Produtos/Repository/ProdutosRepository.java
+++ src/main/java/API_Gerenciado_De_Produtos/Repository/ProdutosRepository.java
@@ -1,9 +1,11 @@
 package API_Gerenciado_De_Produtos.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 
 import API_Gerenciado_De_Produtos.model.Produtos;
+
+@Repository
 public interface ProdutosRepository extends JpaRepository <Produtos, Long>{
 
 }
