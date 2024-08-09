1f4b5179dd931e91599a4b7ade720594df264f0e
BryanLucasCabral <bryan.brasil.bl@gmail.com>
2024-08-08 21:29:07 -0300

    Criado metodo de salvarProduto

--- src/main/java/API_Gerenciado_De_Produtos/services/ProdutosServices.java
+++ src/main/java/API_Gerenciado_De_Produtos/services/ProdutosServices.java
@@ -4,9 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import API_Gerenciado_De_Produtos.Repository.ProdutosRepository;
+import API_Gerenciado_De_Produtos.model.Produtos;
 
 @Service
 public class ProdutosServices {
     @Autowired
     private ProdutosRepository produtosRepository;
+
+    public Produtos salvarProdutos(Produtos produtos){
+        return produtosRepository.save(produtos);
+    }
 }
