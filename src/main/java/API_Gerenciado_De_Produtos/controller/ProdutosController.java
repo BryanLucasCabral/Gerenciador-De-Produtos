4c6af467cc18f3ef459f397e9544189e32553f4f
BryanLucasCabral <bryan.brasil.bl@gmail.com>
2024-08-10 10:19:38 -0300

    Criado endpoint para listar usuarios

--- src/main/java/API_Gerenciado_De_Produtos/controller/ProdutosController.java
+++ src/main/java/API_Gerenciado_De_Produtos/controller/ProdutosController.java
@@ -1,13 +1,19 @@
 package API_Gerenciado_De_Produtos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import API_Gerenciado_De_Produtos.dto.ProdutosDTO;
 import API_Gerenciado_De_Produtos.model.Produtos;
 import API_Gerenciado_De_Produtos.services.ProdutosServices;
 
@@ -21,4 +27,10 @@ public class ProdutosController {
     public ResponseEntity<Produtos> salvaProdutos(@RequestBody Produtos produtos){
         return ResponseEntity.status(HttpStatus.OK).body(produtosServices.salvarProdutos(produtos));
     } 
+
+    @GetMapping
+    public ResponseEntity<Page<ProdutosDTO>> listarUsuarios(
+        @PageableDefault(size = 10, page = 1, sort = "nome", direction = Direction.DESC) Pageable paginacao){
+            return ResponseEntity.status(HttpStatus.OK).body(produtosServices.listarUsuarios(paginacao));
+        }
 }
