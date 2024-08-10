cf8e79197e590c80e0ae32b3b30f7d19de6a0087
BryanLucasCabral <bryan.brasil.bl@gmail.com>
2024-08-10 10:44:43 -0300

    Criado endpoint para deletar Usuario

--- src/main/java/API_Gerenciado_De_Produtos/controller/ProdutosController.java
+++ src/main/java/API_Gerenciado_De_Produtos/controller/ProdutosController.java
@@ -10,7 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,5 +48,17 @@ public class ProdutosController {
         }
 
         return ResponseEntity.status(HttpStatus.OK).body(produtos);
+    }
+
+    @DeleteMapping("id")
+    public ResponseEntity<Void> deletarProdutos(@PathVariable("id") Long id){
+        ProdutosDTO produtos = produtosServices.buscarProdutoPeloId(id);
+
+        if (Objects.isNull(produtos)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        produtosServices.deletarProdutos(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
