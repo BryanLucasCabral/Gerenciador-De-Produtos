5d067ca9ba741ccff1cbf02229915b62f7344c18
BryanLucasCabral <bryan.brasil.bl@gmail.com>
2024-08-10 10:09:56 -0300

    Adicionando atributos a ProdutosDTO

--- src/main/java/API_Gerenciado_De_Produtos/dto/ProdutosDTO.java
+++ src/main/java/API_Gerenciado_De_Produtos/dto/ProdutosDTO.java
@@ -1,5 +1,14 @@
 package API_Gerenciado_De_Produtos.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProdutosDTO {
-    
+    private Long id;
+    private String nome;
+    private String descricao;
 }
