d7777056262d96c82e38026b9c41699d29a07244
BryanLucasCabral <bryan.brasil.bl@gmail.com>
2024-08-14 21:36:17 -0300

    Criado repository de fornecedores

---
+++ src/main/java/API_Gerenciado_De_Produtos/Repository/FornecedorRepository.java
@@ -0,0 +1,13 @@
+package API_Gerenciado_De_Produtos.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import API_Gerenciado_De_Produtos.model.Fornecedor;
+
+/**
+ * FornecedorRepository
+ */
+public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
+
+    
+}
