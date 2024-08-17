8c6d9a931c9ee3b0dbde6573d9556768163b3052
BryanLucasCabral <bryan.brasil.bl@gmail.com>
2024-08-17 10:36:23 -0300

    Criado endpoint para buscar fornecedor pelo id

--- src/main/java/API_Gerenciado_De_Produtos/controller/FornecedorController.java
+++ src/main/java/API_Gerenciado_De_Produtos/controller/FornecedorController.java
@@ -1,5 +1,7 @@
 package API_Gerenciado_De_Produtos.controller;
 
+import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,10 +9,13 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import API_Gerenciado_De_Produtos.dto.FornecedorDTO;
 import API_Gerenciado_De_Produtos.model.Fornecedor;
 import API_Gerenciado_De_Produtos.services.FornecedoresServices;
 
@@ -26,5 +31,16 @@ public class FornecedorController {
 
     public ResponseEntity<Page<Fornecedor>> listarFornecedores(@PageableDefault(size = 10, page = 1, sort = "nome", direction = Direction.DESC) Pageable paginacao){
         return ResponseEntity.status(HttpStatus.OK).body(fornecedoresServices.listarFornecedores(paginacao));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FornecedorDTO> buscarFornecedor(@PathVariable("id") Long id){
+        FornecedorDTO fornecedor = fornecedoresServices.buscarFornecedorPeloId(id);
+
+        if (Objects.isNull(fornecedor)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(fornecedor);
     }
 }
