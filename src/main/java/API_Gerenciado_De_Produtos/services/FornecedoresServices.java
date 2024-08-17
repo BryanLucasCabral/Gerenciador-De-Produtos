dfe571f0b8d95b107f5e5b2b96ea33948d55b722
BryanLucasCabral <bryan.brasil.bl@gmail.com>
2024-08-17 10:17:36 -0300

    Criado metodo para buscar fornecedor pelo id

--- src/main/java/API_Gerenciado_De_Produtos/services/FornecedoresServices.java
+++ src/main/java/API_Gerenciado_De_Produtos/services/FornecedoresServices.java
@@ -1,11 +1,14 @@
 package API_Gerenciado_De_Produtos.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import API_Gerenciado_De_Produtos.Repository.FornecedorRepository;
+import API_Gerenciado_De_Produtos.dto.FornecedorDTO;
 import API_Gerenciado_De_Produtos.model.Fornecedor;
 
 @Service
@@ -21,11 +24,31 @@ public class FornecedoresServices {
         return fornecedorRepository.findAll(paginacao);
     }
 
+    public FornecedorDTO buscarFornecedorPeloId(Long id){
+        Optional<Fornecedor> fornecedorOpt = fornecedorRepository.findById(id);
+
+        if (fornecedorOpt.isPresent()) {
+            return fornecedorOpt.get().toDTO();
+        }
+        return null;
+    }
+
     public void deletarFornecedores(Long id){
         fornecedorRepository.findById(id);
     }
 
     public Fornecedor atualizarFornecedor(Long id,Fornecedor dadosFornecedor){
+        Optional<Fornecedor> fornecedorOpt = fornecedorRepository.findById(id);
         
+        if (fornecedorOpt.isPresent()) {
+            Fornecedor fornecedor = fornecedorOpt.get();
+
+            fornecedor.setNome(dadosFornecedor.getNome());
+            fornecedor.setCnpj(dadosFornecedor.getCnpj());
+
+            return fornecedorRepository.save(fornecedor);
+        }
+
+        return null;
     }
 }
