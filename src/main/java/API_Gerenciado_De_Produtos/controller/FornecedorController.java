eff0e247188542b1d7c933b200f450ccdc63fc79
BryanLucasCabral <bryan.brasil.bl@gmail.com>
2024-08-17 10:43:29 -0300

    Criado endpoint para deletar fornecedor

--- src/main/java/API_Gerenciado_De_Produtos/controller/FornecedorController.java
+++ src/main/java/API_Gerenciado_De_Produtos/controller/FornecedorController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -26,16 +27,17 @@ public class FornecedorController {
     @Autowired
     private FornecedoresServices fornecedoresServices;
 
-    public ResponseEntity<Fornecedor> salvarFornecedor(@RequestBody Fornecedor fornecedor){
+    public ResponseEntity<Fornecedor> salvarFornecedor(@RequestBody Fornecedor fornecedor) {
         return ResponseEntity.status(HttpStatus.CREATED).body(fornecedoresServices.salvarFornecedor(fornecedor));
     }
 
-    public ResponseEntity<Page<Fornecedor>> listarFornecedores(@PageableDefault(size = 10, page = 1, sort = "nome", direction = Direction.DESC) Pageable paginacao){
+    public ResponseEntity<Page<Fornecedor>> listarFornecedores(
+            @PageableDefault(size = 10, page = 1, sort = "nome", direction = Direction.DESC) Pageable paginacao) {
         return ResponseEntity.status(HttpStatus.OK).body(fornecedoresServices.listarFornecedores(paginacao));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<FornecedorDTO> buscarFornecedor(@PathVariable("id") Long id){
+    public ResponseEntity<FornecedorDTO> buscarFornecedor(@PathVariable("id") Long id) {
         FornecedorDTO fornecedor = fornecedoresServices.buscarFornecedorPeloId(id);
 
         if (Objects.isNull(fornecedor)) {
@@ -46,7 +48,7 @@ public class FornecedorController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Fornecedor> atualizarFornecedor(@PathVariable("id") Long id, Fornecedor dadosFornecedor){
+    public ResponseEntity<Fornecedor> atualizarFornecedor(@PathVariable("id") Long id, Fornecedor dadosFornecedor) {
         FornecedorDTO fornecedor = fornecedoresServices.buscarFornecedorPeloId(id);
 
         if (Objects.isNull(fornecedor)) {
@@ -55,5 +57,17 @@ public class FornecedorController {
 
         dadosFornecedor.setId(id);
         return ResponseEntity.status(HttpStatus.OK).body(fornecedoresServices.atualizarFornecedor(id, dadosFornecedor));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarFornecedor(@PathVariable("id") Long id){
+        FornecedorDTO fornecedor = fornecedoresServices.buscarFornecedorPeloId(id);
+
+        if (Objects.isNull(fornecedor)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }   
+
+        fornecedoresServices.deletarFornecedores(id);       
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
