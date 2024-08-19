c52921ed0bbe076c8ebceb5fd65753b23c4c698e
BryanLucasCabral <bryan.brasil.bl@gmail.com>
2024-08-19 19:56:14 -0300

    Metodo de busca de fornecedor pelo nome

--- src/main/java/API_Gerenciado_De_Produtos/Repository/FornecedorRepository.java
+++ src/main/java/API_Gerenciado_De_Produtos/Repository/FornecedorRepository.java
@@ -3,6 +3,8 @@ package API_Gerenciado_De_Produtos.Repository;
 import java.util.Optional;
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import API_Gerenciado_De_Produtos.model.Fornecedor;
@@ -12,6 +14,6 @@ import API_Gerenciado_De_Produtos.model.Fornecedor;
  */
 public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
     Optional<Fornecedor> findByCnpj(String cnpj);
-
     
+    Page<Fornecedor> findByNomeContains(String nome, Pageable pageable);
 }
