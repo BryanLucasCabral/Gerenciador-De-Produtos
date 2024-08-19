0ac7c6a9d4e17e5a592f804df78a1967b7139d71
BryanLucasCabral <bryan.brasil.bl@gmail.com>
2024-08-19 19:52:39 -0300

    Metodo de busca do fornecedor pelo cnpj

--- src/main/java/API_Gerenciado_De_Produtos/Repository/FornecedorRepository.java
+++ src/main/java/API_Gerenciado_De_Produtos/Repository/FornecedorRepository.java
@@ -1,5 +1,8 @@
 package API_Gerenciado_De_Produtos.Repository;
 
+import java.util.Optional;
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import API_Gerenciado_De_Produtos.model.Fornecedor;
@@ -8,6 +11,7 @@ import API_Gerenciado_De_Produtos.model.Fornecedor;
  * FornecedorRepository
  */
 public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
+    Optional<Fornecedor> findByCnpj(String cnpj);
 
     
 }
