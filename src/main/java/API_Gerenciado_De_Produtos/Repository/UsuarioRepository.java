d987a019574c9dd2b3f007ffaa4bf71a37aa2ea0
BryanLucasCabral <bryan.brasil.bl@gmail.com>
2024-08-19 20:36:33 -0300

    Criado procura nome de usuario no repository

--- src/main/java/API_Gerenciado_De_Produtos/Repository/UsuarioRepository.java
+++ src/main/java/API_Gerenciado_De_Produtos/Repository/UsuarioRepository.java
@@ -1,5 +1,9 @@
 package API_Gerenciado_De_Produtos.Repository;
 
+
+import java.util.Optional;
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import API_Gerenciado_De_Produtos.model.Usuario;
@@ -9,5 +13,5 @@ import API_Gerenciado_De_Produtos.model.Usuario;
  */
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
     
-    
+    Optional<Usuario> findByNomeContains(String nome);
 }
