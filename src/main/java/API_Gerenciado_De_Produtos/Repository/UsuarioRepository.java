a7b5016dc683c999fc25a3703dea8db1c8db80f0
BryanLucasCabral <bryan.brasil.bl@gmail.com>
2024-08-19 20:44:10 -0300

    Alteração no tipo de retorno do método

--- src/main/java/API_Gerenciado_De_Produtos/Repository/UsuarioRepository.java
+++ src/main/java/API_Gerenciado_De_Produtos/Repository/UsuarioRepository.java
@@ -1,7 +1,7 @@
 package API_Gerenciado_De_Produtos.Repository;
 
 
-import java.util.Optional;
+import java.util.List;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,5 +13,5 @@ import API_Gerenciado_De_Produtos.model.Usuario;
  */
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
     
-    Optional<Usuario> findByNomeContains(String nome);
+   List<Usuario> findByNomeContains(String nome);
 }
